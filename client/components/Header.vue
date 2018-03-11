9a63527dd60eb3490bef7efb5648c2001f64fac0
Damaera <damaera@live.com>
2018-03-11 16:42:13 +0700

    edit header text

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -3,12 +3,26 @@
     <div class="header-flex">
       Framenote.
     </div>
+    <!-- <a
+      :href="downloadData"
+      download="data.json"
+    >
+      <button>
+        Download All Content
+      </button>
+    </a> -->
   </div>
 </template>
 
 <script>
 export default {
   computed: {
+    downloadData () {
+      const vuexData = localStorage.getItem('vuex')
+      return "data:text/json;charset=utf-8," + encodeURI(JSON.stringify(vuexData));
+    }
+  },
+  methods: {
   }
 }
 </script>
